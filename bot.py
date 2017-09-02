1a86b62f9d36ec44b103bdc5ce53297ed56fe860
Alexander Terry <alexterry43@gmail.com>
2017-09-02 15:15:48 -0800

    clean up some convert some strings to fstrings use fewer imports

--- bot.py
+++ bot.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from discord.ext import commands
-import discord
-import config
-import json
 import os
+
+from discord.ext import commands
+
+import config
+
 
 class Bot(commands.Bot):
     def __init__(self, **kwargs):
         super().__init__(
-            command_prefix=commands.when_mentioned_or('alex!' if os.uname().nodename == 'alexlaptop' else 'a!'),
+            command_prefix=('alex!' if os.uname().nodename == 'alexlaptop' else 'a!'),
             **kwargs)
 
         for cog in config.cogs:
             try:
                 self.load_extension(cog)
             except Exception as e:
-                print('Could not load extension {0} due to {1.__class__.__name__}: {1}'.format(cog, e))
+                print(f'Could not load extension {cog} due to {e.__class__.__name__}: {e}')
 
     async def on_ready(self):
-        print('Logged on as {0} (ID: {0.id})'.format(self.user))
+        print(f'Logged on as {self.user} (ID: {self.user.id})')
+
 
 
 bot = Bot()
 
 # write general commands here
-
-
 
 bot.run(config.token)
