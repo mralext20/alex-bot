c90381c8a025cbc4122d7cf0e6f1acd030fdf7c2
Alexander Terry <alexterry43@gmail.com>
2017-09-25 19:05:49 -0800

    start converting to mongo...

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ except AssertionError:
 
 try:
     import config
-    import psycopg2
+    import pymongo
 except ImportError(config):
     leave("you need to make a config. please read the README.md for help.")
-except ImportError(psycopg2):
+except ImportError(pymongo):
     leave("you need to install the requirements.")
 
 
@@ -27,10 +27,20 @@ for i in [config.dsn, config.token]:
     except AssertionError:
         leave("please fill in the config file.")
 
-cur = psycopg2.connect(config.dsn).cursor()
+mongo = pymongo.MongoClient(config.mongo)
 
-cur.execute("""CREATE TABLE tags (hash TEXT PRIMARY KEY, name TEXT, guild BIGINT, content TEXT, author BIGINT)""")
-print("created tags table")
-print("created the tags DB")
+db = mongo.alexbot
 
-cur.execute("""CREATE TABLE todo (hash TEXT PRIMARY KEY, name TEXT, content TEXT, author BIGINT)""")
+tags = db.tags
+todo = db.todo
+
+tags.insert_one({"NAME":"hello world",
+                 "CONTENT":"Its nice to meet you.",
+                 "GUILD":295341979800436736,
+                 "AUTHOR":80351110224678912,
+                 "HASH": "1ca25c85001011127a3db6712b5e425b4ad4672c9754535b81e72f99c784112e"})
+
+todo.insert_one({"NAME": "hello world",
+                 "CONTENT":"this is a todo reminder for alex!",
+                 "AUTHOR":108429628560924672,
+                 "HASH": "59c09526346af2ce22dfb318bebc532736483b88ad2e2cbf41b9377a2528efec"})
