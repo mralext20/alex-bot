eddde4393a8ac1b014dc4e03fce5db8d6f9fb59f
FrostLuma <FrostLuma@gmail.com>
2017-10-02 23:53:28 +0200

    improve command error handling, send unexpected errors to chat

--- cogs/errors.py
+++ cogs/errors.py
@@ -1,57 +1,50 @@
 import traceback
-import sys
-import discord.ext
+
+import discord
+from discord.ext import commands
+
 from cogs.cog import Cog
 
 
 class CommandErrorHandler(Cog):
 
-
-    async def on_command_error(self, ctx, error):
-        """The event triggered when an error is raised while invoking a command.
-        ctx   : Context
-        error : Exception"""
-
-        if isinstance(error, discord.ext.commands.CommandNotFound):
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        """The event triggered when an error is raised while invoking a command."""
+        if isinstance(error, commands.CommandNotFound):
             return
 
+        msg = None
+
+        if isinstance(error, commands.DisabledCommand):
+            msg = f'{ctx.command} has been disabled.'
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            msg = f'{ctx.command} can not be used in Private Messages.'
+
+        elif isinstance(error, commands.BadArgument):
+            msg = f'Bad argument! {error} See {ctx.prefix}help {ctx.command} for help!'
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            msg = f'Parameter {error.param} is required but missing! See {ctx.prefix}help {ctx.command} for help!'
+
+        elif isinstance(error, commands.CommandInvokeError):
+            error = error.original
+
+            if isinstance(error, discord.Forbidden):
+                msg = 'A permission error occured while executing this command! ' \
+                      'Make sure I have the required permissions and try again.'
+
+        # post the error into the chat if no short error message could be generated
+        if msg is None:
+            trace = traceback.format_exception(type(error), error, error.__traceback__, limit=7)
+            actual_trace = '\n'.join(trace)
+            msg = f'Something unexpected went wrong while running this command:\n```py\n{actual_trace}```'
+
         try:
-            if isinstance(error.original, discord.errors.Forbidden):
-                await ctx.send(f'**I do not have the required permissions to run this command.**')
-        except AttributeError:
-            pass
-
-        if isinstance(error, discord.ext.commands.DisabledCommand):
-            try:
-                await ctx.send(f'{ctx.command} has been disabled.')
-            except:
-                pass
-            finally:
-                return
-
-        if isinstance(error, discord.ext.commands.NoPrivateMessage):
-            try:
-                await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
-            except:
-                pass
-            finally:
-                return
-
-        if isinstance(error, discord.ext.commands.BadArgument):
-            if ctx.command.qualified_name == 'tag list':
-                return await ctx.send('I could not find that member. Please try again.')
-
-        if isinstance(error, discord.ext.commands.MissingRequiredArgument):
-            try:
-                await ctx.send(f"{ctx.command} requires something to go after it, please provide that.")
-            except:
-                pass
-            finally:
-                return
-
-        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+            await ctx.send(msg)
+        except discord.HTTPException:
+            pass  # /shrug
 
 
-def setup(bot):
+def setup(bot: commands.Bot):
     bot.add_cog(CommandErrorHandler(bot))
