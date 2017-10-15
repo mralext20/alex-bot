8bb9a53f7e09460ff870d7f6d80cafd73e2995b7
Alexander Terry <alexterry43@gmail.com>
2017-10-14 22:35:38 -0800

    NOT NULL

--- setup.py
+++ setup.py
@@ -34,16 +34,21 @@ except psycopg2.Error:
 
 # build tables
 cur.execute("""CREATE TABLE IF NOT EXISTS tags 
-               (hash TEXT PRIMARY KEY, name TEXT, guild BIGINT, content TEXT, author BIGINT)""")
+               (hash TEXT PRIMARY KEY NOT NULL,
+               tag TEXT NOT NULL,
+               guild BIGINT NOT NULL, 
+               content TEXT NOT NULL, 
+               author BIGINT NOT NULL)""")
 
 cur.execute("""CREATE TABLE IF NOT EXISTS configs (guild BIGINT PRIMARY KEY, currency BOOL NOT NULL DEFAULT True)""")
 
 # initial tag from b1nZ for tags
 
-cur.execute("""INSERT INTO tags (hash, name, guild, content) VALUES 
+cur.execute("""INSERT INTO tags (hash, tag, guild, content, author) VALUES 
             ('1ca25c85001011127a3db6712b5e425b4ad4672c9754535b81e72f99c784112e',
             'hello world',
             295341979800436736,
+            'hello, im ghost of b1nzy who wrote this tag',
             80351110224678912""")
 
 # initial config for memework
