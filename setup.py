b6d9488236b80fcdbe190d5e8b8bd01e58ff57d3
Alexander Terry <alexterry43@gmail.com>
2017-10-15 22:03:05 -0800

    Move schema to own file
    (thanks frostluma for suggestion)

--- setup.py
+++ setup.py
@@ -33,25 +33,7 @@ except psycopg2.Error:
     leave("uh ur auth is wrong kiddo, or smthin")
 
 # build tables
-cur.execute("""CREATE TABLE IF NOT EXISTS tags 
-               (hash TEXT PRIMARY KEY NOT NULL,
-               tag TEXT NOT NULL,
-               guild BIGINT NOT NULL, 
-               content TEXT NOT NULL, 
-               author BIGINT NOT NULL)""")
-
-cur.execute("""CREATE TABLE IF NOT EXISTS configs (guild BIGINT PRIMARY KEY, currency BOOL NOT NULL DEFAULT True)""")
-
-# initial tag from b1nZ for tags
-
-cur.execute("""INSERT INTO tags (hash, tag, guild, content, author) VALUES 
-            ('1ca25c85001011127a3db6712b5e425b4ad4672c9754535b81e72f99c784112e',
-            'hello world',
-            295341979800436736,
-            'hello, im ghost of b1nzy who wrote this tag',
-            80351110224678912""")
-
-# initial config for memework
-cur.execute("""INSERT INTO configs (guild, currency) VALUES (295341979800436736, True)""")
+with open('schema.sql', 'r') as f:
+    cur.execute(f)
 
 print("Done!")
