1527e72f5f044c6289c99b0630b9f805eb03d927
Alexander Terry <alexterry43@gmail.com>
2017-11-05 02:17:34 -0900

    ayy -> lmao

--- alexBot/cogs/fun.py
+++ alexBot/cogs/fun.py
@@ -1,7 +1,11 @@
 from discord.ext import commands
+
 from ..tools import Cog
 from ..tools import get_json
+
+
 import discord
+
 
 
 class Fun(Cog):
@@ -12,6 +16,16 @@ class Fun(Cog):
         ret = discord.Embed()
         ret.set_image(url=cat['file'])
         await ctx.send(embed=ret)
+
+
+    async def on_message(self, message):
+        # TODO: convert to decorator, such that @in_guild(GUILD_ID) works
+        if message.guild.id != 212076640589709315:
+            return
+        ayygen = ('ayy' + 'y' * x for x in range(20))
+        if message.content in ayygen:
+            await message.channel.send("lmao")
+
 
 
 def setup(bot):
