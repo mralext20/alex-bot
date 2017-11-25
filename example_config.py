0859b8709ce05388175b5ec1dca2c8c5cd137971
Alexander Terry <alexterry43@gmail.com>
2017-11-25 14:08:09 -0900

    [thx @FrostLuma] channel based logging

--- example_config.py
+++ example_config.py
@@ -6,4 +6,8 @@ dsn = 'dbname="alexbot" user="postgres" password="password"'
 
 MONEY = {'CHANCE': .05, 'PER_MESSAGE': choice(range(0, 100)) / 100, "REACTION": "\n{MONEY-MOUTH FACE}"}
 
-
+logging = {
+    'info': 'webhook url here',
+    'warning': 'webhook here too',
+    'error': 'webhook url here three',
+}
