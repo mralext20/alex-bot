a8938b10526e2da4b556b5b6a583443155a7f23b
Alexander Terry <alexterry43@gmail.com>
2017-12-10 17:43:16 -0800

    add guild configs

--- alexBot/tools.py
+++ alexBot/tools.py
@@ -3,6 +3,8 @@ from asyncpg.pool import Pool
 import logging
 import json
 
+from discord.ext import commands
+
 log = logging.getLogger(__name__)
 
 configKeys = {
@@ -38,15 +40,41 @@ async def haste(session: aiohttp.ClientSession, text: str, extension: str = "py"
         return ret
 
 
-async def create_guild_config(pool: Pool, guild_id: int) -> dict:
+async def create_guild_config(bot, guild_id: int) -> dict:
     cfg = json.dumps(configKeys)
-    await pool.execute("""INSERT INTO configs (id, data, type) VALUES ($1, $2, 'guild')""", guild_id, cfg)
-    return await get_guild_config(pool, guild_id)
+    await bot.pool.execute("""INSERT INTO configs (id, data, type) VALUES ($1, $2, 'guild')""", guild_id, cfg)
+    return await get_guild_config(bot, guild_id)
 
 
-async def get_guild_config(pool: Pool, guild_id: int) -> dict:
-    ret = await pool.fetchrow("""SELECT data FROM configs WHERE id=$1 AND type='guild'""", guild_id)
-    if ret is None:
-        return await create_guild_config(pool, guild_id)
-    ret = json.loads(ret[0])
+async def get_guild_config(bot, guild_id: int) -> dict:
+    ret = {}
+    try:
+        ret = bot.configs[guild_id]
+    except KeyError:
+        ret = await bot.pool.fetchrow("""SELECT data FROM configs WHERE id=$1 AND type='guild'""", guild_id)
+        if ret is None:
+            ret = await create_guild_config(bot, guild_id)
+        ret = json.loads(ret[0])
+    finally:
+        ret = {**configKeys, **ret}
+        bot.configs[guild_id] = ret
     return ret
+
+async def update_guild_key(bot, guild_id:int, key: str, value):
+    """updates the `key` to be `value`.
+    note: this method is extreamly dumb,
+    as it does no error checking to ensure that you are giving it the right value for a key."""
+    bot.configs[guild_id][key] = value
+    cfg = json.dumps(bot.configs[guild_id])
+    await bot.pool.execute("""UPDATE configs SET data=$1 WHERE id=$2""",cfg, guild_id)
+
+
+
+class BoolConverter(commands.Converter):
+    async def convert(self, ctx, argument):
+        if argument[0].lower() == "f":
+            return False
+        elif argument[0].lower() == "t":
+            return True
+        else:
+            raise commands.BadArgument(f'can not convert {argument} to True or False')
