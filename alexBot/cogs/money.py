d8ab6d10352bb810697bda5e5b061f0bf7ba6542
Alexander Terry <alexterry43@gmail.com>
2017-12-16 17:27:55 -0900

    now a way to aquire money

--- alexBot/cogs/money.py
+++ alexBot/cogs/money.py
@@ -3,10 +3,17 @@
 from discord.ext import commands
 import discord
 from ..tools import Cog
+from random import choice
+import logging
+import asyncio
 
 from ..tools import get_wallet
 from ..tools import get_guild_config
 from ..tools import update_wallet
+from ..tools import TransactionError
+
+log = logging.getLogger(__name__)
+
 
 class Money(Cog):
     """The description for Money goes here."""
@@ -14,7 +21,6 @@ class Money(Cog):
         super().__init__(*args, **kwargs)
 
         self.lock = False
-
 
     @commands.command()
     @commands.is_owner()
@@ -29,7 +35,40 @@ class Money(Cog):
         if target is None:
             target = ctx.author
         wallet = await get_wallet(self.bot,target.id)
-        await ctx.send(f"wallet for {target} is {wallet}")
+        await ctx.send(f"wallet for {target} is {wallet:.2f}")
+
+
+    @commands.command()
+    async def transfer(self, ctx, target:discord.Member, amount:float):
+        """transfers an amount of money from your wallet to the target's wallet."""
+        author_wallet = await get_wallet(self.bot, ctx.author.id)
+        target_wallet = await get_wallet(self.bot, target.id)
+        try:
+            assert author_wallet > amount
+        except AssertionError:
+            raise TransactionError("you don't have enough funds for that")
+        await update_wallet(self.bot, ctx.author.id, author_wallet-amount)
+        await update_wallet(self.bot, target.id, target_wallet+amount)
+        # TODO: make a modify_wallet func that takes a float and applies that value to the target_wallet when you do that also use it on line #67
+        await ctx.send(f"sent {amount} to {target.display_name}")
+
+
+    async def on_message(self, message:discord.Message):
+        try:
+            gcfg = await get_guild_config(self.bot, message.guild.id)
+            assert gcfg['money'] == True
+        except (AssertionError, AttributeError):
+            # not in guild
+            return
+        chance = choice(range(0, 100)) / 100 # get this message's percent chance
+        if chance < self.bot.config.money['CHANCE']:
+            log.info(f'gave {message.author} money')
+            await message.add_reaction(self.bot.config.money['REACTION'])
+            old = await get_wallet(self.bot, message.author.id)
+            await update_wallet(self.bot, message.author.id, old + self.bot.config.money['PER_MESSAGE'])
+            await asyncio.sleep(5)
+            await message.remove_reaction(self.bot.config.money['REACTION'], self.bot.user)
+            return
 
 
 def setup(bot):
