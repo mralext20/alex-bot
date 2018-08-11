978d443fc0a38866c65500ce3aea18f7170eda82
Alexander Terry <alexterry43@gmail.com>
2018-08-11 13:55:50 -0800

    remove check out new parts from metar command

--- alexBot/cogs/weather.py
+++ alexBot/cogs/weather.py
@@ -189,11 +189,7 @@ class Weather(Cog):
 
         if 'metar' not in display_type:
             embed.timestamp = report_time
-        if display_type is 'normal':
-            await ctx.send("Check out new readable and raw only displays! see a!help metar for details,"
-                           " or join my server (a!about) and ask for help there.", embed=embed)
-        else:
-            await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
