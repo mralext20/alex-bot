4b60fa4535b71b2bf8ee4b026a0fd4e5e5c16a0e
Alexander Terry <alexterry43@gmail.com>
2020-04-10 17:25:18 -0600

    indentation

--- alexBot/cogs/highlighter.py
+++ alexBot/cogs/highlighter.py
@@ -12,9 +12,12 @@ class Highlighter(Cog):
     async def on_message(self, message):
         if self.bot.location == 'dev' or message.guild is None:
             return
-      if message.guild.id in self.bot.config.listenServers:
-        if any(each in message.content for each in self.bot.config.listens):
-          await _bot.owner.send(f"highlight: {messasge.author.mention} ({message.author}) in {message.channel.mention} ({message.channel})\n{message.jump_url}\n\n{message.content}")
+        if message.guild.id in self.bot.config.listenServers:
+            if any(each in message.content for each in self.bot.config.listens):
+                await self.bot.owner.send(
+                    f"highlight: {message.author.mention}({message.author})"
+                    f"in {message.channel.mention}({message.channel})"
+                    f"\n{message.jump_url}\n\n{message.content}")
 
 
 def setup(bot):
