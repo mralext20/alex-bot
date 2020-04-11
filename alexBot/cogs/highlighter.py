d89ddbb3ab70de19982bf8b1d04e5b8857fde07b
Alexander Terry <alexterry43@gmail.com>
2020-04-10 18:30:26 -0600

    dont highlighht for my own messages

--- alexBot/cogs/highlighter.py
+++ alexBot/cogs/highlighter.py
@@ -14,7 +14,7 @@ class Highlighter(Cog):
     async def on_message(self, message):
         if self.bot.location == 'dev' or message.guild is None:
             return
-        if message.guild.id in self.bot.config.listenServers:
+        if message.guild.id in self.bot.config.listenServers and message.author.id != self.bot.owner.id:
             if any(each in message.content for each in self.bot.config.listens):
                 await self.bot.owner.send(
                     f"highlight: {message.author.mention}({message.author})"
