b8fbce103c47205254aa6f49fbac65112a22b39d
Alexander Terry <alexterry43@gmail.com>
2020-11-21 19:11:25 -0900

    add v.redd.it support to tiktok embeding

--- alexBot/cogs/tiktok.py
+++ alexBot/cogs/tiktok.py
@@ -11,6 +11,7 @@ from youtube_dl import YoutubeDL
 
 log = logging.getLogger(__name__)
 tiktok = re.compile(r'https?://vm\.tiktok\.com/.{6,}/')
+reddit = re.compile(r'https?://v\.redd\.it/.{13,}')
 opts = {'outtmpl': 'out.mp4'}
 ytdl = YoutubeDL(opts)
 
@@ -19,9 +20,9 @@ class TikTok(Cog):
     active = False
 
     @staticmethod
-    def download_tiktok(url):
+    def download_tiktok(url, source):
         data = ytdl.extract_info(url, download=True)
-        return data['title']
+        return data['title'] if source == 'tiktok' else ''
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -29,9 +30,14 @@ class TikTok(Cog):
             return
         if (await get_guild_config(self.bot, message.guild.id))['tikTok'] is False:
             return
+        match_from = 'tiktok'
         matches = tiktok.match(message.content)
         if matches is None:
-            return
+            matches = reddit.match(message.content)
+            match_from = 'reddit'
+            if matches is None:
+                return
+
         match = matches.group(0)
         log.info(f'collecting {match} for {message.author}')
 
@@ -45,7 +51,7 @@ class TikTok(Cog):
                     await message.add_reaction('⌛')
                 except discord.Forbidden:
                     pass
-                task = partial(self.download_tiktok, match)
+                task = partial(self.download_tiktok, match, match_from)
                 title = await self.bot.loop.run_in_executor(None, task)
                 if os.path.getsize('out.mp4') > 8000000:
                     try:
@@ -55,7 +61,7 @@ class TikTok(Cog):
                         await message.channel.send('too large too send')
                 else:
                     # file is out.mp4, need to create discord.File and upload it to channel then delete out.mp4
-                    file = discord.File('out.mp4', 'tiktok.mp4')
+                    file = discord.File('out.mp4', 'tiktok.mp4' if match_from == 'tiktok' else 'vid.mp4')
                     await message.channel.send(title, file=file)
                     await message.remove_reaction('⌛', self.bot.user)
                     await message.add_reaction('✅')
