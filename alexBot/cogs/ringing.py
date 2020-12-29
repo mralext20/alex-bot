8c4a3197cd701990182cded35f11ce19d0751a1d
Alexander Terry <alexterry43@gmail.com>
2020-12-28 19:52:15 -0900

    ringing works!

--- alexBot/cogs/ringing.py
+++ alexBot/cogs/ringing.py
@@ -1,6 +1,7 @@
 import asyncio
 import typing
 import discord
+from discord.errors import DiscordException
 
 from alexBot.tools import Cog, get_user_config
 from discord.ext import commands
@@ -9,9 +10,9 @@ from discord.ext import commands
 class Ringing(Cog):
     @commands.command()
     async def ring(self, ctx: commands.Context, target: discord.Member):
-        if not ctx.author.voice:
-            await ctx.send("cannot ring: you are not in a voice channel")
-            return
+        # if not ctx.author.voice:
+        #     await ctx.send("cannot ring: you are not in a voice channel")
+        #     return
         if target.voice:
             await ctx.send("cannot ring: they are already in voice")
             return
@@ -23,6 +24,10 @@ class Ringing(Cog):
         await ctx.message.add_reaction("❌")
         await self.doRing(ctx.author.name, target, ctx.channel, ctx.message, ringRate)
         await ctx.message.remove_reaction("❌", self.bot.user)
+        try:
+            await ctx.message.clear_reactions()
+        except DiscordException:
+            pass
         await ctx.message.add_reaction("✅")
 
     @staticmethod
@@ -33,7 +38,7 @@ class Ringing(Cog):
                      ringRate={"times": 1, "rate": 1},
                      ):
         times = 0
-        while (not target.voice) and times < ringRate['times'] and (sentinalMessage.reactions[0].count < 2):
+        while (not target.voice) and times < ringRate['times'] and ((await sentinalMessage.channel.fetch_message(sentinalMessage.id)).reactions[0].count < 2):
             await channel.send(f"HELLO, {target.mention}! {initiator.upper()} WANTS YOU TO JOIN VOICE!")
             await asyncio.sleep(ringRate['rate'])
             times += 1
