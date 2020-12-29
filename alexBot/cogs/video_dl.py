2788f640bf58805a62bfe5ad43d3cd0b000bca2a
Alexander Terry <alexterry43@gmail.com>
2020-12-28 17:54:18 -0900

    reformat

--- alexBot/cogs/video_dl.py
+++ alexBot/cogs/video_dl.py
@@ -21,13 +21,13 @@ REGEXES = [
 
 ytdl = YoutubeDL({'outtmpl': 'out.mp4'})
 
-TARGET_SHRINK_SIZE = (8*10**6 - 128*1000) * 8 # 8 MB - 128 KB in bits
-MAX_VIDEO_LENGTH = 5 * 60 # 5 Minutes
-AUDIO_BITRATE = 64 * 1000 # 64 Kbits
-BUFFER_CONSTANT = 20 # Magic number, see https://unix.stackexchange.com/a/598360
+TARGET_SHRINK_SIZE = (8 * 10**6 - 128 * 1000) * 8  # 8 MB - 128 KB in bits
+MAX_VIDEO_LENGTH = 5 * 60  # 5 Minutes
+AUDIO_BITRATE = 64 * 1000  # 64 Kbits
+BUFFER_CONSTANT = 20  # Magic number, see https://unix.stackexchange.com/a/598360
 
 FFPROBE_CMD = 'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 in.mp4'
-FFMPEG_CMD  = 'ffmpeg -i in.mp4 -y -b:v {0} -maxrate:v {0} -b:a {1} -maxrate:a {1} -bufsize:v {2} out.mp4'
+FFMPEG_CMD = 'ffmpeg -i in.mp4 -y -b:v {0} -maxrate:v {0} -b:a {1} -maxrate:a {1} -bufsize:v {2} out.mp4'
 
 
 class Video_DL(Cog):
@@ -36,7 +36,7 @@ class Video_DL(Cog):
     @staticmethod
     def download_video(url):
         data = ytdl.extract_info(url, download=True)
-        return data['title'] 
+        return data['title']
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -44,14 +44,16 @@ class Video_DL(Cog):
             return
         if (await get_guild_config(self.bot, message.guild.id))['tikTok'] is False:
             return
-        
+
         matches = None
 
         for regex in REGEXES:
             matches = regex.match(message.content)
-            if matches: break
+            if matches:
+                break
 
-        if matches is None: return
+        if matches is None:
+            return
 
         match = matches.group(0)
         log.info(f'collecting {match} for {message.author}')
@@ -66,25 +68,27 @@ class Video_DL(Cog):
                         await message.add_reaction('⌛')
                     except discord.Forbidden:
                         pass
-                    
+
                     task = partial(self.download_video, match)
                     title = await self.bot.loop.run_in_executor(None, task)
-                    
+
                     if os.path.getsize('out.mp4') > 8000000:
                         try:
                             await message.add_reaction('🪄')
-                        except: pass
+                        except:
+                            pass
 
                         await self.bot.loop.run_in_executor(None, self.transcode_shrink)
-                    
+
                     # file is out.mp4, need to create discord.File and upload it to channel then delete out.mp4
                     file = discord.File('out.mp4', 'vid.mp4')
 
                     await message.channel.send(title, file=file)
-                    
+
                     try:
                         await message.add_reaction('✅')
-                    except: pass
+                    except:
+                        pass
 
             except Exception as e:
                 log.warn(f'Exception occurred processing video {e}')
@@ -101,7 +105,6 @@ class Video_DL(Cog):
                 if os.path.exists('out.mp4'):
                     os.remove('out.mp4')
 
-
     @staticmethod
     def transcode_shrink():
         shutil.copyfile('out.mp4', 'in.mp4')
@@ -110,14 +113,14 @@ class Video_DL(Cog):
         try:
             input_filesize = os.path.getsize('in.mp4')
             video_length = float(subprocess.check_output(FFPROBE_CMD.split(' ')).decode("utf-8"))
-            
+
             if video_length > MAX_VIDEO_LENGTH:
                 raise commands.CommandInvokeError('Video is too large.')
 
             target_total_bitrate = TARGET_SHRINK_SIZE / video_length
             buffer_size = math.floor(TARGET_SHRINK_SIZE / BUFFER_CONSTANT)
             target_video_bitrate = target_total_bitrate - AUDIO_BITRATE
-                
+
             command_formatted = FFMPEG_CMD.format(
                 str(target_video_bitrate),
                 str(AUDIO_BITRATE),
@@ -125,13 +128,14 @@ class Video_DL(Cog):
             )
 
             subprocess.check_call(command_formatted.split(' '))
-            
+
         except Exception as e:
             log.warn(f'Exception occurred transcoding video {e}')
 
         finally:
             if os.path.exists('in.mp4'):
                 os.remove('in.mp4')
+
 
 def setup(bot):
     bot.add_cog(Video_DL(bot))
