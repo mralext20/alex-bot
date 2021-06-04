63d2a9626e646f8b5c210c7da663e1b02c52a533
Alexander Terry <alexterry43@gmail.com>
2021-06-03 18:44:42 -0800

    move stealEmoji to fun

--- alexBot/cogs/fun.py
+++ alexBot/cogs/fun.py
@@ -4,6 +4,7 @@ import time
 from typing import Dict
 
 import discord
+from discord import PartialEmoji
 from discord.ext import commands
 
 from ..tools import Cog, get_json
@@ -14,6 +15,29 @@ ayygen = re.compile("[aA][yY][Yy][yY]*")
 
 class Fun(Cog):
     last_posted: Dict[int, float] = {}
+
+    EMOJI_REGEX = re.compile(r"<(?P<animated>a?):(?P<name>[a-zA-Z0-9_]{2,32}):(?P<id>[0-9]{18,22})>")
+
+    @commands.command(aliases=["emojiSteal"])
+    @commands.is_owner()
+    async def stealEmoji(self, ctx: commands.Context, index: int = 0):
+        if not ctx.message.reference:
+            raise commands.BadArgument("you need to reply to a message")
+        target = ctx.message.reference.resolved
+        raw_emojis = self.EMOJI_REGEX.findall(target.content)
+        emojis = [
+            PartialEmoji.with_state(self.bot._connection, animated=(e[0] == 'a'), name=e[1], id=e[2])
+            for e in raw_emojis
+        ]
+
+        emoji = emojis[index]
+        data = await emoji.url.read()
+        nerdiowo = ctx.bot.get_guild(791528974442299412)
+        uploaded = await nerdiowo.create_custom_emoji(name=emoji.name, image=data)
+        try:
+            await ctx.reply(f"{uploaded}")
+        except discord.errors.Forbidden:
+            await ctx.author.send(f"{uploaded}")
 
     @commands.command()
     async def cat(self, ctx: commands.Context):
--- alexBot/cogs/utils.py
+++ alexBot/cogs/utils.py
@@ -7,7 +7,6 @@ import re
 import discord
 import humanize
 from discord.ext import commands
-from discord.partial_emoji import PartialEmoji
 
 from ..tools import Cog
 
@@ -16,8 +15,6 @@ DATEFORMAT = "%a, %e %b %Y %H:%M:%S (%-I:%M %p)"
 
 class Utils(Cog):
     """The description for Utils goes here."""
-
-    EMOJI_REGEX = re.compile(r"<(?P<animated>a?):(?P<name>[a-zA-Z0-9_]{2,32}):(?P<id>[0-9]{18,22})>")
 
     @commands.command(aliases=['p'])
     async def ping(self, ctx: commands.Context):
@@ -93,27 +90,6 @@ class Utils(Cog):
         await ctx.send(
             f"<https://discord.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot%20applications.commands>"
         )
-
-    @commands.command()
-    @commands.is_owner()
-    async def stealEmoji(self, ctx: commands.Context, index: int = 0):
-        if not ctx.message.reference:
-            raise commands.BadArgument("you need to reply to a message")
-        target = ctx.message.reference.resolved
-        raw_emojis = self.EMOJI_REGEX.findall(target.content)
-        emojis = [
-            PartialEmoji.with_state(self.bot._connection, animated=(e[0] == 'a'), name=e[1], id=e[2])
-            for e in raw_emojis
-        ]
-
-        emoji = emojis[index]
-        data = await emoji.url.read()
-        nerdiowo = ctx.bot.get_guild(791528974442299412)
-        uploaded = await nerdiowo.create_custom_emoji(name=emoji.name, image=data)
-        try:
-            await ctx.reply(f"{uploaded}")
-        except discord.errors.Forbidden:
-            await ctx.author.send(f"{uploaded}")
 
     @commands.command()
     @commands.has_guild_permissions(move_members=True)
