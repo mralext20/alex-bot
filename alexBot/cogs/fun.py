e4d90e025cf371524721b26023264eb0f9ccae27
Alexander Terry <alexterry43@gmail.com>
2022-07-27 20:11:06 -0800

    validate targets

--- alexBot/cogs/fun.py
+++ alexBot/cogs/fun.py
@@ -115,6 +115,11 @@ class Fun(Cog):
                 ephemeral=True,
             )
             return
+        valid_targets = [m for m in interaction.user.voice.channel.members if not m.bot and not m.id == interaction.user.id]
+        if len(valid_targets) == 0:
+            await interaction.response.send_message("noone is in this channel except bots", ephemeral=True)
+            return
+
 
         current = interaction.user.voice.channel
         target = interaction.guild.afk_channel
@@ -124,7 +129,7 @@ class Fun(Cog):
                 max_values=1,
                 options=[
                     discord.SelectOption(label=m.display_name, value=str(m.id))
-                    for m in interaction.user.voice.channel.members
+                    for m in valid_targets
                 ],
             )
 
