bce3bdc2a55b0985f4c5bf384676b748aec30647
Alexander Terry <alexterry43@gmail.com>
2023-01-12 21:42:55 -0900

    mcStatus as slash command

--- alexBot/cogs/minecraft.py
+++ alexBot/cogs/minecraft.py
@@ -3,9 +3,10 @@ import re
 
 import discord
 import mcstatus
-from discord.ext import commands
+from discord import app_commands
+from typing import Optional
 
-from ..tools import Cog, get_json
+from ..tools import Cog
 
 log = logging.getLogger(__name__)
 
@@ -13,23 +14,24 @@ REMOVE_SECTION = re.compile("(\U000000a7.)")
 
 
 class Minecraft(Cog):
-    @commands.command()
-    async def mcStatus(self, ctx: commands.Context, *, server: str = None):
+    @app_commands.command()
+    async def mcStatus(self, interaction: discord.Interaction, server: Optional[str] = None):
         # a!mcStatus
         # -> guild.Minecraft
-        if server is None and ctx.guild is not None:
-            gd = await self.bot.db.get_guild_data(ctx.guild.id)
+        if server is None and interaction.guild is not None:
+            gd = await self.bot.db.get_guild_data(interaction.guild.id)
             server = gd.config.minecraft if gd.config.minecraft else None
             if server is None:
-                return await ctx.send(
-                    "please provide a server via argument or with `config set guild.Minecraft <SERVER>`"
+                return await interaction.response.send_message(
+                    "please provide a server via argument or with `config set guild.Minecraft <SERVER>`",
+                    ephemeral=True,
                 )
-        try:
 
+        try:
             mcserver = mcstatus.MinecraftServer.lookup(server)
             status = await mcserver.async_status()
-        except Exception:
-            return await ctx.send("an error occured, the server may be down..")
+        except mcstatus:
+            return await interaction.response.send_message("an error occured, the server may be down..")
 
         embed = discord.Embed(description=REMOVE_SECTION.sub('', status.description['text']))
 
@@ -39,7 +41,7 @@ class Minecraft(Cog):
             embed.add_field(name="players", value='\n'.join([player.name for player in status.players.sample]))
         embed.add_field(name="Online / Max", value=f"{status.players.online} / {status.players.max}")
 
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
 
 async def setup(bot):
