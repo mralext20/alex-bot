a8e0fdc6a0b053eabc1aefb4c0d6ee99856dae8e
Alexander Terry <alexterry43@gmail.com>
2023-01-14 20:18:06 -0900

    ringing slash command

--- alexBot/cogs/ringing.py
+++ alexBot/cogs/ringing.py
@@ -1,70 +1,68 @@
 import asyncio
 
 import discord
-from discord.ext import commands
-
+from discord import app_commands
+from typing import Dict
 from alexBot.classes import RingRate
 from alexBot.tools import Cog
 
+def mk_callback(task: asyncio.Task):
+    async def callback(interaction: discord.Interaction):
+        task.cancel()
+        await interaction.response.send_message("canceled ringing", ephemeral=True)
+    return callback
+
 
 class Ringing(Cog):
-    @commands.max_concurrency(1, per=commands.BucketType.channel)
-    @commands.cooldown(1, 5 * 60, commands.BucketType.user)
-    @commands.command()
-    async def ring(self, ctx: commands.Context, target: discord.Member):
+    class CancelableTaskView(discord.ui.View):
+        def __init__(self, task: asyncio.Task):
+            super().__init__(timeout=240)
+            btn = discord.ui.Button(label="cancel", style=discord.ButtonStyle.danger)
+            btn.callback = mk_callback(task)
+            self.add_item(btn)
+
+    @app_commands.command()
+    @app_commands.guild_only()
+    async def ring(self, interaction: discord.Interaction, target: discord.Member):
         """Alerts another member of the server that you want someone to talk to. requires that you're in a voice channel."""
-        if not ctx.author.voice:
-            await ctx.send("cannot ring: you are not in a voice channel")
-            ctx.command.reset_cooldown(ctx)
+        if not interaction.user.voice:
+            await interaction.response.send_message("cannot ring: you are not in a voice channel", ephemeral=True)
             return
 
         if target.voice:
-            await ctx.send("cannot ring: they are already in voice")
-            ctx.command.reset_cooldown(ctx)
+            await interaction.response.send_message("cannot ring: they are already in voice", ephemeral=True)
             return
 
         if not (await self.bot.db.get_user_data(target.id)).config.ringable:
-            await ctx.send("cannot ring: they do not want to be rung")
-            ctx.command.reset_cooldown(ctx)
+            await interaction.response.send_message("cannot ring: they do not want to be rung", ephemeral=True)
             return
 
         ringRate = self.bot.config.ringRates[target.status]
-        await ctx.message.add_reaction("❌")
-        await self.doRing(ctx.author, target, ctx.channel, ctx.message, ringRate)
-        await ctx.message.add_reaction("✅")
+        task = asyncio.create_task(self.doRing(interaction.user, target, interaction.channel, ringRate))
+        await interaction.response.send_message("ringing...", view=self.CancelableTaskView(task))
+        try:
+            await task
+        except asyncio.CancelledError:
+            pass
+        await (await interaction.original_response()).edit(view=None)
 
     async def doRing(
         self,
         initiator: discord.Member,
         target: discord.Member,
         channel: discord.TextChannel,
-        sentinalMessage: discord.Message,
         ringRate: RingRate = RingRate(),
     ):
         times = 0
         allowed_mentions = discord.AllowedMentions(users=[target])
 
-        while await self.running(target, times, ringRate, sentinalMessage):
+        while times < ringRate.times:
             await channel.send(
                 f"HELLO, {target.mention}! {initiator.name.upper()} WANTS YOU TO JOIN {initiator.voice.channel.mention}!",
                 allowed_mentions=allowed_mentions,
             )
             await asyncio.sleep(ringRate.rate)
             times += 1
-
-    @staticmethod
-    async def running(target: discord.Member, times: int, ringRate: RingRate, sentinalMessage: discord.Message):
-        if target.voice:
-            return False
-        if times >= ringRate.times:
-            return False
-
-        newSentinalMessage = await sentinalMessage.channel.fetch_message(sentinalMessage.id)
-
-        if not newSentinalMessage.reactions:
-            return False
-        return newSentinalMessage.reactions[0].count < 2
-
 
 async def setup(bot):
     await bot.add_cog(Ringing(bot))
