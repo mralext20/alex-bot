fce47023249893053897ecd0eb432c49351592eb
Alexander Terry <alexterry43@gmail.com>
2023-02-10 21:33:08 -0900

    set log level?

--- bot.py
+++ bot.py
@@ -29,6 +29,7 @@ cogs = [
 
 log = logging.getLogger(__name__)
 handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
 handler.setFormatter(
     logging.Formatter('[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 )
