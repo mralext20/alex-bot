0551f552e1ceed9d05a9a57f9e85bc96dcf1f8d9
Alexander Terry <alexterry43@gmail.com>
2023-02-17 22:32:58 -0900

    oopsps

--- alexBot/cogs/smartHome.py
+++ alexBot/cogs/smartHome.py
@@ -26,11 +26,10 @@ members = {'alex': 108429628560924672, 'garrett': 326410251546918913}
 
 class PhoneMonitor(Cog):
     @Cog.listener()
-    async def on_ha_update_location(self, name:str, location: PayloadType):
+    async def on_ha_update_location(self, name: str, location: PayloadType):
         print('HERE!')
         log.info(f"HA update: {name} -> {location}")
         await self.bot.wait_until_ready()
-        name = 
 
         if name in members:
             g = self.bot.get_guild(GUILD)
@@ -43,7 +42,6 @@ class PhoneMonitor(Cog):
             log.info(f"Changing {member.display_name} to {name}")
             await member.edit(nick=name)
 
-
     @Cog.listener()
     async def on_ha_vc_control(self, name: str, command: PayloadType):
         log.info(f"HA vc control: {name} -> {command}")
@@ -51,7 +49,7 @@ class PhoneMonitor(Cog):
         g = self.bot.get_guild(GUILD)
         assert g is not None
         if name in members:
-            member= g.get_member(members[name])
+            member = g.get_member(members[name])
             if not member or not member.voice:
                 return
             if command == 'mute':
@@ -60,6 +58,7 @@ class PhoneMonitor(Cog):
                 await member.edit(deafen=not member.voice.deaf)
             elif command == 'disconnect':
                 await member.move_to(None)
+
 
 async def setup(bot: "Bot"):
     await bot.add_cog(PhoneMonitor(bot))
