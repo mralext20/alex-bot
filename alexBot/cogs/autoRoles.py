ed6eb2f9549b76fc17b5ca62d11a65216490310a
Alexander Terry <alexterry43@gmail.com>
2023-03-10 08:53:10 -0900

    that's a stupid bug

--- alexBot/cogs/autoRoles.py
+++ alexBot/cogs/autoRoles.py
@@ -57,7 +57,7 @@ class autoRoles(Cog):
             ButtonType.GAME: discord.ui.View(timeout=None),
             ButtonType.PHONE: discord.ui.View(timeout=None),
             ButtonType.INTEREST: discord.ui.View(timeout=None),
-            ButtonType.PROUNOUNS: discord.ui.View(timeout=None),
+            ButtonType.PRONOUNS: discord.ui.View(timeout=None),
         }
         self.flat_roles = await self.bot.db.get_roles_data()
         for type in ButtonType:
