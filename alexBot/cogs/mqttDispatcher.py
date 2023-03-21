eb78cf254eb21fcbc069c09f9f6fd64e4a5265fe
Alexander Terry <alexterry43@gmail.com>
2023-03-20 21:36:38 -0800

    reconnect logic for mqtt

--- alexBot/cogs/mqttDispatcher.py
+++ alexBot/cogs/mqttDispatcher.py
@@ -1,26 +1,37 @@
 # -*- coding: utf-8 -*-
 
+import asyncio
+import logging
 import asyncio_mqtt as aiomqtt
 
 from ..tools import Cog
+
+log = logging.getLogger(__name__)
 
 
 class HomeAssistantIntigreation(Cog):
     def __init__(self, bot):
         super().__init__(bot)
-        self.task = None
+        self.task: asyncio.Task = None
 
     async def mqttLoop(self):
-        async with aiomqtt.Client(**self.bot.config.mqttServer) as client:
-            async with client.messages() as messages:
-                await client.subscribe("alex-bot/#")
-                async for message in messages:
-                    if message.topic.matches("alex-bot/location/#"):
-                        self.bot.dispatch(
-                            "ha_update_location", message.topic.value.split('/')[2], message.payload.decode()
-                        )
-                    if message.topic.matches("alex-bot/vcControl/#"):
-                        self.bot.dispatch("ha_vc_control", message.topic.value.split('/')[2], message.payload.decode())
+        while True:
+            try:
+                async with aiomqtt.Client(**self.bot.config.mqttServer) as client:
+                    async with client.messages() as messages:
+                        await client.subscribe("alex-bot/#")
+                        async for message in messages:
+                            if message.topic.matches("alex-bot/location/#"):
+                                self.bot.dispatch(
+                                    "ha_update_location", message.topic.value.split('/')[2], message.payload.decode()
+                                )
+                            if message.topic.matches("alex-bot/vcControl/#"):
+                                self.bot.dispatch(
+                                    "ha_vc_control", message.topic.value.split('/')[2], message.payload.decode()
+                                )
+            except aiomqtt.MqttError as error:
+                log.error(f"MQTT error: {error}, retrying in 5 minutes")
+                await asyncio.sleep(5 * 60)
 
     async def mqttPublish(self, topic, payload):
         async with aiomqtt.Client(**self.bot.config.mqttServer) as client:
