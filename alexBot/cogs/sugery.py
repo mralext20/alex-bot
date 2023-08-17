0dd70e12797bbff5037b4caf4ef6d4743ff7968d
Alexander Terry <alexterry43@gmail.com>
2023-08-17 09:43:37 -0800

    commit in loop?

--- alexBot/cogs/sugery.py
+++ alexBot/cogs/sugery.py
@@ -172,7 +172,7 @@ class Sugery(Cog):
                         veryLow=t['bgLow'],
                     )
                     session.add(user)
-            await session.commit()
+                    await session.commit()
             await self.bot.wait_until_ready()
 
     def cog_unload(self):
