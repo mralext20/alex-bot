252ffd6cbfd090e260cf0a5989f650d4aa33e7b4
Alexander Terry <alexterry43@gmail.com>
2023-09-25 19:24:17 -0800

    correct device name

--- alexBot/cogs/smartHome.py
+++ alexBot/cogs/smartHome.py
@@ -40,7 +40,7 @@ MEMBERS = {  # ha-name: (discord-id, [guild-ids])
 NEWLINE = '\n'
 USER_TO_HA_DEVICE = {
     108429628560924672: 'mobile_app_pixel_7_pro',
-    326410251546918913: 'mobile_app_knyte_s_iphone',
+    326410251546918913: 'mobile_app_knyte_s_phone',
     253233185800847361: 'mobile_app_egg_2',
 }
 
